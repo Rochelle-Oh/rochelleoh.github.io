eecf1c3cf1fb5ec9e2eb133814d4a5901ab52d5b
Rochelle-Oh <ohr21235@gmail.com>
2019-08-31 16:29:51 +1000

    Animations and pop ups

--- future-signals/assets/js/main.js
+++ future-signals/assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	console.log("The page is ready!");
 
+//NAV FILTERS
 	// Toggle large research item
 	$('.research-item').click(function(){
 		$(this).toggleClass('large')
@@ -47,4 +48,63 @@ $(document).ready(function(){
 		$('.research-item').removeClass('hide');
 	});
 
+});
+
+//not safe
+
+$(function(){  // $(document).ready shorthand
+  $('.notsafe').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.notsafe').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},200);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
+//POP UP WINDOW
+$(function(){  // $(document).ready shorthand
+  $('.popup').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.popup').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},2000);
+                    
+            }
+            
+        }); 
+    
+    });
+    
 });
