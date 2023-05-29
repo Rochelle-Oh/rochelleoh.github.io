2349cda39dd19aa6b08a84dd9f29b39fc120ee39
rochelleoh <rochieohmg@gmail.com>
2023-05-29 16:42:19 +1000

    Youtube iframes and Webms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
 	console.log("The page is ready!");
 
+	var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
 // Grid filter active class
 
 	$('.filter-item').click(function(){
